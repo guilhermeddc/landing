aee947c8aba810c4d22eb20d86e1179aa8397c7a
Guilherme Rodrigues <guilhermeddc@gmail.com>
2021-05-13 21:23:50 -0300

    prod

--- src/pages/Feedback/index.tsx
+++ src/pages/Feedback/index.tsx
@@ -5,6 +5,7 @@ import {
   Box,
   Button,
   Container,
+  Fade,
   Paper,
   TextField,
   Typography,
@@ -83,184 +84,186 @@ const Feedback: React.FC = () => {
   );
 
   return (
-    <Box
-      minHeight="100vh"
-      display="flex"
-      alignItems="center"
-      justifyContent="center">
-      <Container maxWidth="md" style={{marginTop: 16, marginBottom: 16}}>
-        <form onSubmit={handleSubmit}>
-          <Box
-            display="flex"
-            flexDirection="column"
-            justifyContent="center"
-            alignItems="center"
-            height="100%"
-            padding="32px 32px 64px"
-            component={Paper}>
-            <img src={mock.logo || Logo} alt="logo" width="250px" />
-
-            <Box
-              width="100%"
-              maxWidth="420px"
-              height="1px"
-              marginBottom={4}
-              style={{backgroundColor: theme.palette.primary.main}}
-            />
-
-            <Typography variant="h6" align="center">
-              {mock.question}
-            </Typography>
-
+    <Fade in={true} timeout={500}>
+      <Box
+        minHeight="100vh"
+        display="flex"
+        alignItems="center"
+        justifyContent="center">
+        <Container maxWidth="md" style={{marginTop: 16, marginBottom: 16}}>
+          <form onSubmit={handleSubmit}>
             <Box
               display="flex"
+              flexDirection="column"
               justifyContent="center"
-              gridGap="8px"
-              flexWrap="wrap"
-              marginY="32px">
-              <ScoreButton
-                value={recommend}
-                setValue={setRecommend}
-                score={0}
-                color="#EF778D"
-              />
-              <ScoreButton
-                value={recommend}
-                setValue={setRecommend}
-                score={1}
-                color="#F19587"
-              />
-              <ScoreButton
-                value={recommend}
-                setValue={setRecommend}
-                score={2}
-                color="#F2A384"
-              />
-              <ScoreButton
-                value={recommend}
-                setValue={setRecommend}
-                score={3}
-                color="#F1B27F"
-              />
-              <ScoreButton
-                value={recommend}
-                setValue={setRecommend}
-                score={4}
-                color="#F3C17D"
-              />
-              <ScoreButton
-                value={recommend}
-                setValue={setRecommend}
-                score={5}
-                color="#F4D079"
-              />
-              <ScoreButton
-                value={recommend}
-                setValue={setRecommend}
-                score={6}
-                color="#EFE77A"
-              />
-              <ScoreButton
-                value={recommend}
-                setValue={setRecommend}
-                score={7}
-                color="#D8E97D"
-              />
-              <ScoreButton
-                value={recommend}
-                setValue={setRecommend}
-                score={8}
-                color="#BDE37F"
-              />
-              <ScoreButton
-                value={recommend}
-                setValue={setRecommend}
-                score={9}
-                color="#A5DD81"
-              />
-              <ScoreButton
-                value={recommend}
-                setValue={setRecommend}
-                score={10}
-                color="#91D683"
-              />
-            </Box>
+              alignItems="center"
+              height="100%"
+              padding="32px 32px 64px"
+              component={Paper}>
+              <img src={mock.logo || Logo} alt="logo" width="250px" />
 
-            {recommend > -1 && recommend < 11 && (
-              <>
-                <Typography variant="h6" align="center">
-                  {mock.comment_title}
-                </Typography>
+              <Box
+                width="100%"
+                maxWidth="420px"
+                height="1px"
+                marginBottom={4}
+                style={{backgroundColor: theme.palette.primary.main}}
+              />
 
-                <Box width="100%" maxWidth="420px" marginTop={2}>
-                  <TextField
-                    multiline
-                    fullWidth
-                    variant="outlined"
-                    value={questions.commentTitle}
-                    error={!questions.commentTitle && submitted}
-                    helperText={
-                      !questions.commentTitle &&
-                      submitted &&
-                      'Campo obrigatório.'
-                    }
-                    onChange={(e) =>
-                      setQuestions(
-                        (state) =>
-                          (state = {
-                            ...state,
-                            commentTitle: e.target.value,
-                          }),
-                      )
-                    }
-                    InputProps={{
-                      style: {
-                        minHeight: theme.spacing(16),
-                        display: 'flex',
-                        alignItems: 'flex-start',
-                        justifyContent: 'flex-start',
-                      },
-                    }}
-                  />
-                </Box>
+              <Typography variant="h6" align="center">
+                {mock.question}
+              </Typography>
 
-                <Box width="100%" maxWidth="420px">
-                  {mock.extra_questions.length > 0 &&
-                    mock.extra_questions.map((item, index) => (
-                      <Box marginTop={3} key={item.id}>
-                        <Typography variant="h6" align="center">
-                          {item.question}
-                        </Typography>
-                        <Box marginTop={2} />
-                        <TextField
-                          value={extraQuestions[index].response}
-                          onChange={(e) => handleChangeResponse(e, index)}
-                          multiline
-                          fullWidth
-                          variant="outlined"
-                        />
-                      </Box>
-                    ))}
+              <Box
+                display="flex"
+                justifyContent="center"
+                gridGap="8px"
+                flexWrap="wrap"
+                marginY="32px">
+                <ScoreButton
+                  value={recommend}
+                  setValue={setRecommend}
+                  score={0}
+                  color="#EF778D"
+                />
+                <ScoreButton
+                  value={recommend}
+                  setValue={setRecommend}
+                  score={1}
+                  color="#F19587"
+                />
+                <ScoreButton
+                  value={recommend}
+                  setValue={setRecommend}
+                  score={2}
+                  color="#F2A384"
+                />
+                <ScoreButton
+                  value={recommend}
+                  setValue={setRecommend}
+                  score={3}
+                  color="#F1B27F"
+                />
+                <ScoreButton
+                  value={recommend}
+                  setValue={setRecommend}
+                  score={4}
+                  color="#F3C17D"
+                />
+                <ScoreButton
+                  value={recommend}
+                  setValue={setRecommend}
+                  score={5}
+                  color="#F4D079"
+                />
+                <ScoreButton
+                  value={recommend}
+                  setValue={setRecommend}
+                  score={6}
+                  color="#EFE77A"
+                />
+                <ScoreButton
+                  value={recommend}
+                  setValue={setRecommend}
+                  score={7}
+                  color="#D8E97D"
+                />
+                <ScoreButton
+                  value={recommend}
+                  setValue={setRecommend}
+                  score={8}
+                  color="#BDE37F"
+                />
+                <ScoreButton
+                  value={recommend}
+                  setValue={setRecommend}
+                  score={9}
+                  color="#A5DD81"
+                />
+                <ScoreButton
+                  value={recommend}
+                  setValue={setRecommend}
+                  score={10}
+                  color="#91D683"
+                />
+              </Box>
 
-                  <Box display="flex" justifyContent="flex-end" marginTop={4}>
-                    <Button
+              {recommend > -1 && recommend < 11 && (
+                <>
+                  <Typography variant="h6" align="center">
+                    {mock.comment_title}
+                  </Typography>
+
+                  <Box width="100%" maxWidth="420px" marginTop={2}>
+                    <TextField
+                      multiline
                       fullWidth
-                      variant="contained"
-                      color="primary"
-                      style={{color: 'white'}}
-                      type="submit">
-                      <Typography variant="h6" align="center">
-                        Salvar
-                      </Typography>
-                    </Button>
+                      variant="outlined"
+                      value={questions.commentTitle}
+                      error={!questions.commentTitle && submitted}
+                      helperText={
+                        !questions.commentTitle &&
+                        submitted &&
+                        'Campo obrigatório.'
+                      }
+                      onChange={(e) =>
+                        setQuestions(
+                          (state) =>
+                            (state = {
+                              ...state,
+                              commentTitle: e.target.value,
+                            }),
+                        )
+                      }
+                      InputProps={{
+                        style: {
+                          minHeight: theme.spacing(16),
+                          display: 'flex',
+                          alignItems: 'flex-start',
+                          justifyContent: 'flex-start',
+                        },
+                      }}
+                    />
                   </Box>
-                </Box>
-              </>
-            )}
-          </Box>
-        </form>
-      </Container>
-    </Box>
+
+                  <Box width="100%" maxWidth="420px">
+                    {mock.extra_questions.length > 0 &&
+                      mock.extra_questions.map((item, index) => (
+                        <Box marginTop={3} key={item.id}>
+                          <Typography variant="h6" align="center">
+                            {item.question}
+                          </Typography>
+                          <Box marginTop={2} />
+                          <TextField
+                            value={extraQuestions[index].response}
+                            onChange={(e) => handleChangeResponse(e, index)}
+                            multiline
+                            fullWidth
+                            variant="outlined"
+                          />
+                        </Box>
+                      ))}
+
+                    <Box display="flex" justifyContent="flex-end" marginTop={4}>
+                      <Button
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        style={{color: 'white'}}
+                        type="submit">
+                        <Typography variant="h6" align="center">
+                          Salvar
+                        </Typography>
+                      </Button>
+                    </Box>
+                  </Box>
+                </>
+              )}
+            </Box>
+          </form>
+        </Container>
+      </Box>
+    </Fade>
   );
 };
 
--- src/pages/Form/index.tsx
+++ src/pages/Form/index.tsx
@@ -13,6 +13,7 @@ import {
   Checkbox,
   FormControlLabel,
   Link,
+  Fade,
 } from '@material-ui/core';
 
 import mock from '../../mocks/data.json';
@@ -61,165 +62,169 @@ const Form: React.FC = () => {
   );
 
   return (
-    <Box
-      minHeight="100vh"
-      display="flex"
-      alignItems="center"
-      justifyContent="center">
-      <Container maxWidth="md" style={{marginTop: 16, marginBottom: 16}}>
-        <form onSubmit={handleSubmit}>
-          <Box
-            display="flex"
-            flexDirection="column"
-            justifyContent="center"
-            alignItems="center"
-            flex={1}
-            height="100%"
-            padding="32px 32px 64px"
-            component={Paper}>
-            <img src={mock.logo || Logo} alt="logo" width="250px" />
-
+    <Fade in={true} timeout={500}>
+      <Box
+        minHeight="100vh"
+        display="flex"
+        alignItems="center"
+        justifyContent="center">
+        <Container maxWidth="md" style={{marginTop: 16, marginBottom: 16}}>
+          <form onSubmit={handleSubmit}>
             <Box
-              width="100%"
-              maxWidth="420px"
-              height="1px"
-              marginBottom={4}
-              style={{backgroundColor: theme.palette.primary.main}}
-            />
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+              alignItems="center"
+              flex={1}
+              height="100%"
+              padding="32px 32px 64px"
+              component={Paper}>
+              <img src={mock.logo || Logo} alt="logo" width="250px" />
 
-            <Typography variant="h5" align="center">
-              {mock.intro}
-            </Typography>
+              <Box
+                width="100%"
+                maxWidth="420px"
+                height="1px"
+                marginBottom={4}
+                style={{backgroundColor: theme.palette.primary.main}}
+              />
 
-            <Box width="100%" maxWidth="420px" marginTop={4}>
-              <Box marginBottom={3}>
-                <TextField
-                  label="Nome *"
-                  value={formData.name}
-                  error={!formData.name && submitted}
-                  helperText={
-                    !formData.name && submitted && 'Campo obrigatório.'
-                  }
-                  onChange={(e) =>
-                    setFormData(
-                      (state) =>
-                        (state = {
-                          ...state,
-                          name: e.target.value,
-                        }),
-                    )
-                  }
-                  multiline
-                  fullWidth
-                  variant="outlined"
-                />
-              </Box>
+              <Typography variant="h5" align="center">
+                {mock.intro}
+              </Typography>
 
-              <Box marginBottom={3}>
-                <TextField
-                  label="E-mail"
-                  error={
-                    !formData.email && formData.phone.length === 0 && submitted
-                  }
-                  helperText={
-                    !formData.email &&
-                    formData.phone.length === 0 &&
-                    submitted &&
-                    'Preencha o e-mail ou celular.'
-                  }
-                  value={formData.email}
-                  onChange={(e) =>
-                    setFormData(
-                      (state) =>
-                        (state = {
-                          ...state,
-                          email: e.target.value,
-                        }),
-                    )
-                  }
-                  multiline
-                  fullWidth
-                  variant="outlined"
-                />
-              </Box>
-
-              <Box marginBottom={3}>
-                <InputMask
-                  mask="(99) 99999 9999"
-                  type="tel"
-                  value={formData.phone}
-                  onChange={(e) =>
-                    setFormData(
-                      (state) =>
-                        (state = {
-                          ...state,
-                          phone: e.target.value,
-                        }),
-                    )
-                  }>
-                  {() => (
-                    <TextField
-                      type="tel"
-                      label="Celular"
-                      error={
-                        !formData.phone &&
-                        formData.email.length === 0 &&
-                        submitted
-                      }
-                      helperText={
-                        !formData.phone &&
-                        formData.email.length === 0 &&
-                        submitted &&
-                        'Preencha o e-mail ou celular.'
-                      }
-                      fullWidth
-                      variant="outlined"
-                    />
-                  )}
-                </InputMask>
-              </Box>
-
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    color="primary"
-                    value={formData.privacy}
+              <Box width="100%" maxWidth="420px" marginTop={4}>
+                <Box marginBottom={3}>
+                  <TextField
+                    label="Nome *"
+                    value={formData.name}
+                    error={!formData.name && submitted}
+                    helperText={
+                      !formData.name && submitted && 'Campo obrigatório.'
+                    }
                     onChange={(e) =>
                       setFormData(
                         (state) =>
                           (state = {
                             ...state,
-                            privacy: e.target.checked,
+                            name: e.target.value,
                           }),
                       )
                     }
+                    multiline
+                    fullWidth
+                    variant="outlined"
                   />
-                }
-                label={
-                  <>
-                    Eu li e concordo com a{' '}
-                    <Link href="/"> Política de Privacidade</Link>
-                  </>
-                }
-              />
+                </Box>
 
-              <Box display="flex" justifyContent="flex-end" marginTop={2}>
-                <Button
-                  variant="contained"
-                  fullWidth
-                  color="primary"
-                  style={{color: 'white'}}
-                  type="submit">
-                  <Typography variant="h6" align="center">
-                    Enviar
-                  </Typography>
-                </Button>
+                <Box marginBottom={3}>
+                  <TextField
+                    label="E-mail"
+                    error={
+                      !formData.email &&
+                      formData.phone.length === 0 &&
+                      submitted
+                    }
+                    helperText={
+                      !formData.email &&
+                      formData.phone.length === 0 &&
+                      submitted &&
+                      'Preencha o e-mail ou celular.'
+                    }
+                    value={formData.email}
+                    onChange={(e) =>
+                      setFormData(
+                        (state) =>
+                          (state = {
+                            ...state,
+                            email: e.target.value,
+                          }),
+                      )
+                    }
+                    multiline
+                    fullWidth
+                    variant="outlined"
+                  />
+                </Box>
+
+                <Box marginBottom={3}>
+                  <InputMask
+                    mask="(99) 99999 9999"
+                    type="tel"
+                    value={formData.phone}
+                    onChange={(e) =>
+                      setFormData(
+                        (state) =>
+                          (state = {
+                            ...state,
+                            phone: e.target.value,
+                          }),
+                      )
+                    }>
+                    {() => (
+                      <TextField
+                        type="tel"
+                        label="Celular"
+                        error={
+                          !formData.phone &&
+                          formData.email.length === 0 &&
+                          submitted
+                        }
+                        helperText={
+                          !formData.phone &&
+                          formData.email.length === 0 &&
+                          submitted &&
+                          'Preencha o e-mail ou celular.'
+                        }
+                        fullWidth
+                        variant="outlined"
+                      />
+                    )}
+                  </InputMask>
+                </Box>
+
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      color="primary"
+                      value={formData.privacy}
+                      onChange={(e) =>
+                        setFormData(
+                          (state) =>
+                            (state = {
+                              ...state,
+                              privacy: e.target.checked,
+                            }),
+                        )
+                      }
+                    />
+                  }
+                  label={
+                    <>
+                      Eu li e concordo com a{' '}
+                      <Link href="/"> Política de Privacidade</Link>
+                    </>
+                  }
+                />
+
+                <Box display="flex" justifyContent="flex-end" marginTop={2}>
+                  <Button
+                    variant="contained"
+                    fullWidth
+                    color="primary"
+                    style={{color: 'white'}}
+                    type="submit">
+                    <Typography variant="h6" align="center">
+                      Enviar
+                    </Typography>
+                  </Button>
+                </Box>
               </Box>
             </Box>
-          </Box>
-        </form>
-      </Container>
-    </Box>
+          </form>
+        </Container>
+      </Box>
+    </Fade>
   );
 };
 
